8d5ea4c8c001eac1054fc71dc34ed4fe31be3905
yoni2k <yoni.k81@gmail.com>
2019-06-30 16:02:58 +0300

    Restructure code to work with settings and to allow for much easier unit tests (using different settings)

--- src/translate.py
+++ src/translate.py
@@ -12,74 +12,113 @@ TODOs:
 """
 
 import requests
-
-url = "https://translation.googleapis.com/language/translate/v2"
-
-
-def get_token():
-    """ Returns token to be used when connection to Google API """
-    try:
-        with open('../token.txt', "r") as tokenFile:
-            file_lines = tokenFile.readlines()
-    except Exception:
-        print("ERROR: Unable to read from token file: ")
-        raise
-
-    if len(file_lines) > 1:
-        raise ValueError('There are more than 1 lines in token file')
-    return file_lines[0].strip('\n')
+import json
 
 
-def get_str_to_translate():
-    """ Returns string that should be translated """
-    try:
-        with open('../to-translate.txt', "r") as translateFile:
-            translate_lines = translateFile.readlines()
-    except Exception:
-        print("ERROR: Unable to read from file with string to translate: ")
-        raise
+class GoogleTranslate:
 
-    if len(translate_lines) > 1:
-        ValueError('There are more than 1 lines in translate file')
-    return translate_lines[0].strip('\n')
+    translate_url = "https://translation.googleapis.com/language/translate/v2"
 
+    class SettingNames:
+        name_token_file_path = 'token_file_path'
+        name_string_to_translate_source_file = 'string_to_translate_source_file'
+        name_translate_string_source = 'translate_string_source'
 
-def send_request(req_token, text_to_translate, format_type, source, target):
-    querystring = {"q":        text_to_translate,
-                   "format":   format_type,
-                   "source":   source,
-                   "target":   target}
+        mand_settings_list = (  name_token_file_path,
+                                name_translate_string_source)
 
-    headers = {'Authorization':    f'Bearer {req_token}',
-               'Host':             "translation.googleapis.com"}
-
-    try:
-        return requests.request("GET", url, headers=headers, params=querystring)
-    except Exception:
-        print("ERROR: Got exception when sending data to Google:\n")
-        raise
+        source_types = ("file", "user_input")
 
 
-def get_translation(resp_json):
-    if resp_json.get('data'):
-        return resp_json['data']['translations'][0]['translatedText']
-    elif resp_json.get('error'):
-        return "ERROR: " + resp_json['error']
-    else:
-        return 'ERROR: unknown parsing error of the response: ' + resp_json
+    def get_token(self):
+        """ Returns token to be used when connection to Google API """
+        try:
+            with open(self.settings[self.SettingNames.name_token_file_path], "r") as tokenFile:
+                file_lines = tokenFile.readlines()
+        except Exception:
+            # TODO change to specific exception, gathering exception infos
+            print("ERROR: Unable to read from token file: ")
+            raise
 
+        if len(file_lines) > 1:
+            raise ValueError('There are more than 1 lines in token file')
+        return file_lines[0].strip('\n')
 
-def translate(text_to_translate, format_type, source, target):
-    token = get_token()
-    response = send_request(token, text_to_translate, format_type, source, target)
-    return get_translation(response.json())
+    def get_str_to_translate(self):
+        """ Returns string that should be translated """
+        # TODO do I need this function at all? Like this?
+        if self.settings[self.SettingNames.name_translate_string_source] == "user_input":
+            return input('Enter word to translate from English to Hebrew: ')
+        elif self.settings[self.SettingNames.name_translate_string_source] == "file":
+            return self.settings[self.SettingNames.name_string_to_translate_source_file]
+        else:
+            raise Exception("Invalid source type: " + self.SettingNames.name_translate_string_source)
+        # TODO raise specific exception
+
+    def send_request(self, text_to_translate, format_type, source, target):
+        # TODO write explanation
+        querystring = {"q":        text_to_translate,
+                       "format":   format_type,
+                       "source":   source,
+                       "target":   target}
+
+        headers = {'Authorization':    f'Bearer {self.token}',
+                   'Host':             "translation.googleapis.com"}
+
+        try:
+            return requests.request("GET", self.translate_url, headers=headers, params=querystring)
+        except Exception:
+            print("ERROR: Got exception when sending data to Google:\n")
+            # TODO change to specific exception, gathering exception infos
+            raise
+
+    @staticmethod
+    def parse_translate_response(resp_json):
+        # TODO write explanation
+        if resp_json.get('data'):
+            return resp_json['data']['translations'][0]['translatedText']
+        elif resp_json.get('error'):
+            return "ERROR: " + str(resp_json['error'])
+        else:
+            return 'ERROR: unknown parsing error of the response: ' + resp_json
+
+    def translate(self, text_to_translate, format_type, source, target):
+        # TODO write explanation
+        response = self.send_request(text_to_translate, format_type, source, target)
+        return self.parse_translate_response(response.json())
+
+    def check_all_settings_exist(self):
+        try:
+            for setting_name in self.SettingNames.mand_settings_list:
+                self.settings[setting_name]
+
+            if not self.settings[self.SettingNames.name_translate_string_source] in self.SettingNames.source_types:
+                # TODO change to proper Exception type
+                raise Exception("Invalid source type: " + self.SettingNames.string_to_translate_source_type)
+
+            if (str(self.settings[self.SettingNames.name_translate_string_source])) == "file":
+                self.settings[self.SettingNames.name_string_to_translate_source_file]
+        except Exception as e:
+            # TODO change type of Exception to specific type
+            # TODO make sure that 2nd string really adds additional string
+            raise Exception("Missing one of mandatory settings values: " + str(e))
+
+    def __init__(self, settings_file_path):
+        # TODO write explanation
+        with open(settings_file_path, "r") as settings_file:
+            self.settings = json.load(settings_file)
+        self.check_all_settings_exist()
+        self.token = self.get_token()
 
 
 def main():
     try:
-        text_to_translate = get_str_to_translate()
-        # to_translate = input('Enter word to translate from English to Hebrew: ')
-        translation = translate(text_to_translate, "text", "en", "he")
+        tranl_object = GoogleTranslate("../resources/settings.json")
+
+        # TODO is this how we want to get it?
+        text_to_translate = tranl_object.get_str_to_translate()
+
+        translation = tranl_object.translate(text_to_translate, "text", "en", "he")
         print("Translating \"" + text_to_translate + "\": \"" + translation + "\"")
     except Exception as e:
         print("ERROR while translating: " + str(e))
--- tests/test_translate.py
+++ tests/test_translate.py
@@ -1,5 +1,43 @@
-from src import translate
+import pytest
+from src.translate import GoogleTranslate
+
+def basic_test():
+    tranl_object = GoogleTranslate("../resources/settings.json")
+    assert tranl_object.translate("hello", "text", "en", "he") == "שלום"
 
 
 def test_translate():
-    assert translate.translate("hello", "text", "en", "he") == "שלום"
+    basic_test()
+
+"""
+src_token_location = '../resources/token.txt'
+dest_token_location = src_token_location + '1'
+
+
+@pytest.fixture(name='rename_token_file_back', scope='module')
+def rename_token_file_back(request):
+    print("Yoni in rename_token_file_back")
+
+    def finalizer():
+        print("Yoni in finalizer")
+        os.rename(dest_token_location, src_token_location)
+        basic_test()
+
+    request.addfinalizer(finalizer)
+
+
+@pytest.mark.usefixtures("rename_token_file_back")
+def test_token_file_not_found():
+    print("Yoni test_token_file_not_found called")
+    # make sure things work before file change
+    basic_test()
+
+    # setup - rename file
+    os.rename(src_token_location, dest_token_location)
+    # test
+    with pytest.raises(Exception) as e:
+        print("Yoni got up to here")
+"""
+
+
+
